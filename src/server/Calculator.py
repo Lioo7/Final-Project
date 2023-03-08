9322b793e703c8aa46628170f226e016304c641e
OfirOvadia96 <ofirrr999@gmail.com>
2023-03-08 20:21:20 +0200

    add doctest to Calculator class

--- src/server/Calculator.py
+++ src/server/Calculator.py
@@ -4,23 +4,49 @@ class Calculator:
     '''This static class performing all the calculations'''
     
     @staticmethod
-    def get_voter_count(self) -> float:
-        '''How much people votes'''
+    def get_voter_count(self) -> int:
+        '''
+        How much people votes
         
+        >>> # When nobody votes
+        >>> Calculator.get_voter_count()
+        0
+        >>> # When 200 users votes
+        >>> Calculator.get_voter_count()
+        200
+        '''
         return 0
     
-    @staticmethod
-    def get_voter_count_by_gender() -> float:
-        pass
     
     @staticmethod
-    def get_voter_count_by_age() ->float :
-        pass
+    def get_voter_count_by_gender() -> list[int]:
+        '''
+        Get the number of voters by gender
+        
+        >>> # When nobody votes
+        >>> Calculator.get_voter_count_by_gender()
+        >>> # [MALE, FEMALE]
+        [0, 0]
+        >>>> # When 2 males users votes 
+        >>> Calculator.get_voter_count_by_gender()
+        [2,0]
+        '''
+        return [0, 0]
+
     
     @staticmethod
-    def transferring_the_money_to_the_leaves(tree:Tree) -> None:
-        '''The transfer of the 
-        budget amounts to the leaves'''
-        pass
-    
+    def get_voter_count_by_age() -> list[int]:
+        '''
+        Get the number of voters by age group
+        
+        >>> # When nobody votes
+        >>> Calculator.get_voter_count_by_age()
+        >>> # 18-25, 26-35, 36-45, 46-55, 56-65, 66+
+        [0, 0, 0, 0, 0, 0]
+        >>> # When 2 users votes between 18-25 age 
+        >>> Calculator.get_voter_count_by_age()
+        [2, 0, 0, 0, 0, 0]
+        '''
+        return [0, 0, 0, 0, 0, 0]
+        
     
