4fc375a959c8e25dc7446ff2ccefa7e6753a9bb8
Lioz Akirav <lioz.akirav@gmail.com>
2023-03-26 01:27:39 +0300

    add some utility functions to algorithms

--- src/server/algorithms.py
+++ src/server/algorithms.py
@@ -92,7 +92,10 @@ def median_algorithm(votes: dict) -> dict:
         leaves = find_leaves(node)
         leaves_values[i] = leaves 
 
-    calculate_median(leaves_values)
+    median_values = _calculate_median(leaves_values)
+    result = _create_result(votes, median_values)
+    
+    return result
 
 def generalized_median_algorithm(votes: dict) -> dict:
     """
@@ -195,7 +198,20 @@ def find_leaves(node) -> list:
         
     return leaves
 
-def calculate_median(dict):
+def _calculate_median(dict: dict) -> dict:
+    """
+    A utility function that calculates the median value of each
+    leaf node (project) in a dictionary of budget votes.
+
+    Args:
+        dict (dict): A dictionary where each key represents a user and the value is a list of budget votes 
+            (floats) for each leaf node (project).
+
+    Returns:
+        dict: A dictionary where each key represents a leaf node (project) and the value is its median budget 
+            value across all users.
+    """
+    
     dict_values = {} # {leaf_number (project): its_values (budget)}
     median_values = {} # {leaf_number (project): its_median_value}
     
@@ -209,21 +225,22 @@ def calculate_median(dict):
     for key, values in dict_values.items():
         if key not in median_values:
             median_values[key] = []
-        median = find_median(values)
+        median = _find_median(values)
         median_values[key] = median
     
     return median_values
     
-def find_median(lst) -> float:
+def _find_median(lst: list[float]) -> float:
     """
-    The function returns the median value.
+    A utility function that returns the median value.
 
     Args:
-        lst (List of int): a list of integers.
+        lst (List of floats): a list of integers.
     
     Returns:
         The median number.
     """
+    
     median = 0
     lst_length = len(lst)
     median_index = lst_length // 2
@@ -235,7 +252,103 @@ def find_median(lst) -> float:
         median = (first_median + second_median) / 2
         
     return median
-        
+      
+def _create_result(votes: dict, new_values: list[float]) -> dict:
+    """
+    A utility funfction that create a dictionary that aggregates user votes with the provided budget values.
+    
+    Args:
+    - votes (dict): A dictionary representing the votes of each user, where the keys are the users' IDs and the values
+      are dictionaries representing their votes.
+    - new_values (list[float]): A list of budget values that corresponds to each vote category.
+    
+    Returns:
+    - A dictionary representing the result of aggregating the user votes with the budget values, with keys
+      corresponding to the categories of the votes and nested dictionaries representing subcategories, and values
+      representing the budget allocated to each category or subcategory. The "total" key is added to each nested 
+      dictionary to represent the total budget allocated to the corresponding category or subcategory.
+    """
+    
+    # the key of the first user in the given dict
+    first_key = next(iter(votes))
+    # extracts the vote of the first user
+    first_user_vote = votes[first_key]
+    print(first_user_vote)
+    
+    # create an empty dictionary to hold the final result
+    result = {}
+    # point the current index in the new_values
+    index = {}
+    index[0] = 0
+    
+    result = _building_nested_dict(first_user_vote, new_values, index, result)
+    _calculate_totals(result)
+    
+    return result
+
+def _building_nested_dict(votes: dict, new_values: list, index: list[float], result: dict) -> dict:
+    """
+    A utility function that recursively build a new nested dictionary based on
+    an input dictionary and a list that contains the updated values.
+
+    Args:
+        votes (dict): The input dictionary to build from.
+        new_values (list): A list of values to assign to the keys in the input dictionary.
+        index (list): A list holding a single integer, representing the current index in the list of values.
+        result (dict): A dictionary to add the newly created nested dictionary to.
+
+    Returns:
+        dict: A new nested dictionary built from the input dictionary and list of values.
+    """
+    
+    # create a new dictionary to hold the nested values
+    nested_result = {}
+    
+    # iterate over the items in the input dictionary
+    for key, value in votes.items():
+        # if the current value is a nested dictionary, recursively call the function
+        if isinstance(value, dict):
+            nested_result[key] = _building_nested_dict(value, new_values, index, result)
+        else:
+            # if the current key is "total", leave empty
+            if key == "total":
+                nested_result[key] = ""
+            else:
+                nested_result[key] = new_values[index[0]]
+                index[0] += 1
+    
+    return nested_result
+
+def _calculate_totals(budget: dict) -> float:
+    """
+    A utility function that recursively calculates the total budget
+    for each level of a nested dictionary and assigns it to the 'total' key.
+    The function updates the total keys in the given budget.
+
+    Args:
+        budget (dict): A nested dictionary representing a budget.
+
+    Returns:
+        float: The total budget at the highest level of the dictionary.
+    """
+    
+    total = 0
+    for key, value in budget.items():
+        if isinstance(value, dict):
+            # if the value is a dictionary, recursively calculate its total budget and assign it to the 'total' key
+            sub_dept_budget = _calculate_totals(value)
+            budget[key]['total'] = sub_dept_budget
+            total += sub_dept_budget
+        elif isinstance(value, float):
+            # if the value is a float, add it to the total budget
+            total += value
+    # assign the total budget to the 'total' key of the current level
+    budget['total'] = total
+    
+    return total
+
+
+
 if __name__ == "__main__":
     votes = {
      "user1": {
