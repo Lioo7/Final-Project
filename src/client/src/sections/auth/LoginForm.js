fcfd5c5f26d55b3146fed28f572cfd8dfc425d22
Elhai Mansbach <elhaimn@gmail.com>
2023-03-29 13:53:07 +0300

    Added checks if the user's details are correct

--- src/client/src/sections/auth/LoginForm.js
+++ src/client/src/sections/auth/LoginForm.js
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// @mui
 import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-// components
 import Iconify from '../../components/iconify/Iconify';
 
 export default function LoginForm() {
@@ -12,38 +10,82 @@ export default function LoginForm() {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
 
-  // const handleClick = () => {
-  //   navigate('/peoples_budget/home', { replace: true });
-  // };
+  const [idError, setIdError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const handleClick = async () => {
     const url = 'http://localhost:5000/peoples_budget/login';
-    navigate('/peoples_budget/home', { replace: true });
+
+    // Validate the input fields
+    if (id.length !== 9) {
+      // throw new Error('Invalid ID: Please enter a 9 digit number.');
+      setIdError('Invalid ID: Please enter a 9 digit number.');
+      return;
+    }
+    setIdError('');
+
+    if (password.length < 5) {
+      // throw new Error('Invalid password: Please enter a password with at least 5 characters.');
+      setPasswordError('Invalid password: Please enter a password with at least 5 characters.');
+      return;
+    }
+    setPasswordError('');
 
     try {
       const response = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id: { id }, password: {password} }),
+        body: JSON.stringify({ id, password }),
       });
 
       const responseData = await response.json();
-      console.log(responseData);
+
       if (responseData.status === 'succeed') {
         navigate('/peoples_budget/home', { replace: true });
       } else {
-        throw new Error('Error!, User was not successfully registered');
+        throw new Error('User does not exist. Please register first.');
       }
-
     } catch (error) {
-      console.log(error, 'something');
+      console.log(error);
+      alert(error.message);
     }
   };
+
+  // const handleClick = async () => {
+  //   const url = 'http://localhost:5000/peoples_budget/login';
+  //   // navigate('/peoples_budget/home', { replace: true });
+
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify({ id: { id }, password: {password} }),
+  //     });
+
+  //     const responseData = await response.json();
+  //     console.log(responseData);
+  //     if (responseData.status === 'succeed') {
+  //       navigate('/peoples_budget/home', { replace: true });
+  //     } else {
+  //       throw new Error('Error!, User does not exist');
+  //     }
+
+  //   } catch (error) {
+  //     console.log(error, 'something');
+  //   }
+  // };
 
   return (
     <>
       <Stack spacing={3}>
-        <TextField id="logId" label="ID" value={id} onChange={(e) => setId(e.target.value)} />
+        <TextField
+          id="logId"
+          label="ID"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          error={Boolean(idError)}
+          helperText={idError}
+        />
 
         <TextField
           id="logPassword"
@@ -51,6 +93,8 @@ export default function LoginForm() {
           type={showPassword ? 'text' : 'password'}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          error={Boolean(passwordError)}
+          helperText={passwordError}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -70,7 +114,7 @@ export default function LoginForm() {
         </Link>
       </Stack>
 
-      <LoadingButton id='loginBtn' fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+      <LoadingButton id="loginBtn" fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
         Login
       </LoadingButton>
     </>
--- src/client/src/sections/auth/SignUpForm.js
+++ src/client/src/sections/auth/SignUpForm.js
@@ -1,10 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// @mui
 import { Stack, IconButton, InputAdornment, TextField, MenuItem } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-// import { format, startOfToday } from 'date-fns';
-// components
 import Iconify from '../../components/iconify/Iconify';
 
 export default function SignUpForm() {
@@ -18,15 +15,59 @@ export default function SignUpForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  // const todayDate = format(startOfToday(), 'yyyy-MM-dd');
-
-  // const handleClick = () => {
-  //   navigate('/peoples_budget/home', { replace: true });
-  // };
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+  const [idError, setIdError] = useState('');
+  const [birthDateError, setBirthDateError] = useState('');
+  const [genderError, setGenderError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const handleClick = async () => {
     const url = 'http://localhost:5000/peoples_budget/sign_up';
-    navigate('/peoples_budget/home', { replace: true });
+
+    // Validate the input fields
+    if (firstName === '') {
+      setFirstNameError('Invalid First name: Please enter a first name.');
+      return;
+    }
+    setFirstNameError('');
+
+    if (lastName === '') {
+      setLastNameError('Invalid Last name: Please enter a last name.');
+      return;
+    }
+    setLastNameError('');
+
+    if (id.length !== 9) {
+      setIdError('Invalid ID: Please enter a 9 digit number.');
+      return;
+    }
+    setIdError('');
+
+    if (birthDate === '') {
+      setBirthDateError('Invalid Birth Date: Please enter a birth date.');
+      return;
+    }
+    setBirthDateError('');
+
+    if (gender === '') {
+      setGenderError('Invalid Gender: Please enter a gender.');
+      return;
+    }
+    setGenderError('');
+
+    if (email === '') {
+      setEmailError('Invalid Email: Please enter a email.');
+      return;
+    }
+    setEmailError('');
+
+    if (password.length < 5) {
+      setPasswordError('Invalid password: Please enter a password with at least 5 characters.');
+      return;
+    }
+    setPasswordError('');
 
     try {
       const response = await fetch(url, {
@@ -45,7 +86,7 @@ export default function SignUpForm() {
         }),
       });
 
-      console.log({firstName}, {lastName}, {id},  {birthDate}, {gender}, {email}, {password})
+      console.log({ firstName }, { lastName }, { id }, { birthDate }, { gender }, { email }, { password });
       const responseData = await response.json();
       console.log(responseData);
       if (responseData.status === 'succeed') {
@@ -62,9 +103,30 @@ export default function SignUpForm() {
   return (
     <>
       <Stack spacing={2}>
-        <TextField id="fName" label="First name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        <TextField id="lName" label="Last name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        <TextField id="signId" label="ID" value={id} onChange={(e) => setId(e.target.value)} />
+        <TextField
+          id="fName"
+          label="First name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          error={Boolean(firstNameError)}
+          helperText={firstNameError}
+        />
+        <TextField
+          id="lName"
+          label="Last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          error={Boolean(lastNameError)}
+          helperText={lastNameError}
+        />
+        <TextField
+          id="signId"
+          label="ID"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          error={Boolean(idError)}
+          helperText={idError}
+        />
         <TextField
           id="date"
           label="Birth Date"
@@ -72,22 +134,41 @@ export default function SignUpForm() {
           value={birthDate}
           onChange={(e) => setBirthDate(e.target.value)}
           defaultValue="yyyy-MM-dd"
+          error={Boolean(birthDateError)}
+          helperText={birthDateError}
           // sx={{ width: 220 }}
           InputLabelProps={{
             shrink: true,
           }}
         />
-        <TextField id="gender" label="Gender" select value={gender} onChange={(e) => setGender(e.target.value)}>
+        <TextField
+          id="gender"
+          label="Gender"
+          select
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          error={Boolean(genderError)}
+          helperText={genderError}
+        >
           <MenuItem value="male">Male</MenuItem>
           <MenuItem value="female">Female</MenuItem>
         </TextField>
-        <TextField id="email" label="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <TextField
+          id="email"
+          label="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          error={Boolean(emailError)}
+          helperText={emailError}
+        />
         <TextField
           id="signPassword"
           label="Password"
           type={showPassword ? 'text' : 'password'}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          error={Boolean(passwordError)}
+          helperText={passwordError}
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
@@ -101,7 +182,7 @@ export default function SignUpForm() {
       </Stack>
 
       <Stack sx={{ marginTop: 2 }}>
-        <LoadingButton id='signBtn' fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
+        <LoadingButton id="signBtn" fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
           Sign Up
         </LoadingButton>
       </Stack>
