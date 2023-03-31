6512a59242109bbe51ddc0181457a532f67a438b
Lioz Akirav <lioz.akirav@gmail.com>
2023-03-31 22:13:14 +0300

    refactor algorithms

--- src/server/algorithms.py
+++ src/server/algorithms.py
@@ -16,14 +16,15 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
-def median_algorithm(leaves_values: dict) -> dict:
+def median_algorithm(leaves_values: dict, total_budget: float) -> dict:
     """
     Calculate the median of the votes for the budget and return the budget according to the median votes.
 
     Args
     ----
     leaves_values (dict): A dictionary where each key represents a user and the value is a list of budget votes 
-            (floats) for each leaf node (project).
+        (floats) for each leaf node (project).
+    total_budget (float): The total budget.
             
     Returns
     -------
@@ -82,11 +83,11 @@ def median_algorithm(leaves_values: dict) -> dict:
     True
     """
 
-    median_values = _calculate_median(leaves_values)
+    median_values = _calculate_median(leaves_values, total_budget, 1)
     
     return median_values
 
-def generalized_median_algorithm(leaves_values: dict) -> dict:
+def generalized_median_algorithm(leaves_values: dict, total_budget: float) -> dict:
     """
     Calculate the budget according to the median algorithm of Hervé Moulin, using linear functions by using the given votes.
 
@@ -94,6 +95,7 @@ def generalized_median_algorithm(leaves_values: dict) -> dict:
     ----
     leaves_values (dict): A dictionary where each key represents a user and the value is a list of budget votes 
     (floats) for each leaf node (project).
+    total_budget (float): The total budget.
 
     Returns
     -------
@@ -173,8 +175,9 @@ def generalized_median_algorithm(leaves_values: dict) -> dict:
     True
     """
 
-    # Empty implementation
-    return 0
+    median_values = _calculate_median(leaves_values, total_budget, 2)
+    
+    return median_values
 
 def run_algorithm(votes: dict, algorithm_number: int) -> dict:
     """
@@ -183,7 +186,7 @@ def run_algorithm(votes: dict, algorithm_number: int) -> dict:
     Args:
         votes (dict): A nested dictionary representing the votes of all citizens for the budget.
         algorithm_number (int): The number representing the algorithm to run. 
-                                1 for median_algorithm or 2 for generalized_median_algorithm.
+            1 for median_algorithm or 2 for generalized_median_algorithm.
 
     Raises:
         Exception: If the algorithm_number is not valid.
@@ -197,8 +200,8 @@ def run_algorithm(votes: dict, algorithm_number: int) -> dict:
         print("The dictionary is empty")
         return votes
     
-    # get the number of users
     num_of_users = len(votes)
+    total_budget = votes['user1']['total']
     
     # convert the given dictionary to a tree
     tree = Tree.from_dict(votes)
@@ -213,9 +216,9 @@ def run_algorithm(votes: dict, algorithm_number: int) -> dict:
 
     # choose the algorithm to run based on the input
     if algorithm_number == 1: # median_algorithm
-        algo_values = median_algorithm(leaves_values)
+        algo_values = median_algorithm(leaves_values, total_budget)
     elif algorithm_number == 2: # generalized_median_algorithm
-        algo_values = generalized_median_algorithm(leaves_values)
+        algo_values = generalized_median_algorithm(leaves_values, total_budget)
     else: # wrong input
         raise Exception("Invalid algorithm id!")
     
@@ -238,38 +241,45 @@ def _find_leaves(node) -> list:
         
     return leaves
 
-def _calculate_median(dict: dict) -> dict:
+def _calculate_median(votes_by_user: dict, total_budget: float, algorithm_number: int) -> dict:
     """
     A utility function that calculates the median value of each
     leaf node (project) in a dictionary of budget votes.
 
     Args:
-        dict (dict): A dictionary where each key represents a user and the value is a list of budget votes 
+        votes_by_user (dict): A dictionary where each key represents a user and the value is a list of budget votes 
             (floats) for each leaf node (project).
+        total_budget (float): The total budget.
+        algorithm_number (int): The number representing the algorithm to run. 
+            1 for median_algorithm or 2 for generalized_median_algorithm.
 
     Returns:
         dict: A dictionary where each key represents a leaf node (project) and the value is its median budget 
             value across all users.
     """
     
-    dict_values = {} # {leaf_number (project): its_values (budget)}
+    votes_by_project = {} # {leaf_number (project): its_values (budget)}
     median_values = {} # {leaf_number (project): its_median_value}
     
     # add each value of project (leave) to the dictionary 
-    for user in dict.keys():
-        for count, value in enumerate(list(dict.values())[user]):
-            if count not in dict_values:
-                dict_values[count] = []
-            dict_values[count].append(value)
+    for user in votes_by_user.keys():
+        for count, value in enumerate(list(votes_by_user.values())[user]):
+            if count not in votes_by_project:
+                votes_by_project[count] = []
+            votes_by_project[count].append(value)
     
-    for key, values in dict_values.items():
-        if key not in median_values:
-            median_values[key] = []
-        # sort the list of values
-        sorted_values = sorted(values) 
-        # call _find_median with the sorted list of values
-        median = _find_median(sorted_values) 
-        median_values[key] = median
+    # choose the algorithm to run based on the input
+        if algorithm_number == 2: # generalized_median_algorithm
+            num_of_projects = len(votes_by_project)
+            median = _find_median_with_constant_functions(votes_by_project=votes_by_project, c=total_budget, n=num_of_projects)
+            
+        for key, values in votes_by_project.items():
+            if key not in median_values:
+                median_values[key] = []
+            # sort the list of values
+            sorted_values = sorted(values) 
+            median = _find_median(sorted_values) 
+            median_values[key] = median     
     
     return median_values
     
@@ -295,7 +305,57 @@ def _find_median(lst: list[float]) -> float:
         median = (first_median + second_median) / 2
         
     return median
-      
+
+def _find_median_with_constant_functions(votes_by_project: dict, c: float, n: int, min: float = 0, max: float = 1) -> list[float]:
+    """
+    Find the median of a list of values by adding constant functions.
+
+    Args:
+        votes_by_project (dict): A dictionary where each key represents a project and the value is a list of budget votes 
+            for that project by all the users.
+        c (float): The total budget.
+        n (int): The number of leaves (project).
+        min (float, optional): The minimum value of the search range. Defaults to 0.
+        max (float, optional): The maximum value of the search range. Defaults to 1.
+
+    Returns:
+        List[float]: A list of medians that add up to `c`.
+    """
+    
+    # calculate the midpoint of the search range
+    t = (min + max) / 2 
+    # create an empty list to store the constants
+    constants = []
+
+    # loop 'n' - 1 times to create 'n-1' constants
+    for i in range(1, n):
+        # lreate a new variable name for the constant
+        var_name = "f_" + str(i)
+        constants.append(var_name)
+
+    # calculate the value of each constant using the following formula: f_i = c * min{1, i * t}
+    for i in range(1, n):
+        constants[i-1] = c * min(1, i * t)
+    
+    values_with_constants = values + constants
+    # sort the values and constants 
+    sorted_values_with_constants = sorted(values_with_constants) 
+    # find the medians of the sorted values and constants
+    medians = _find_median(sorted_values_with_constants) 
+    # calculate the sum of the medians
+    sum_medians = sum(medians)
+    
+    # if the sum of the medians is equal to the total budget, return the medians
+    if sum_medians == c:
+        return medians
+    # if the sum of the medians is greater than thetotal budget, search the lower half of the range
+    elif sum_medians > c:
+        return _find_median_with_constant_functions(votes_by_project=votes_by_project, c=c, n=n, min=min, max=t)
+    # If the sum of the medians is less than the total budget, search the upper half of the range
+    elif sum_medians < c:
+        return _find_median_with_constant_functions(votes_by_project=votes_by_project, c=c, n=n, min=t, max=max)
+
+
 def _create_result(votes: dict, new_values: list[float]) -> dict:
     """
     A utility funfction that create a dictionary that aggregates user votes with the provided budget values.
