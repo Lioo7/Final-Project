8b4b0de98a8246887ff9d30c6006dc0e1f2fe7d1
Lioz Akirav <lioz.akirav@gmail.com>
2023-04-08 17:42:22 +0300

    add another approach to store_vote

--- src/server/sql_database.py
+++ src/server/sql_database.py
@@ -352,6 +352,22 @@ class SQL_database(Abstract_Database):
         query = f'''INSERT INTO votes (user_id, vote_data) VALUES ({user_id}, '{vote_str}');'''
         
         try:
+            """
+            TODO: the code below is another approach to store the data by break down the dictionary 
+            into multiple rows and store each level in a separate column in the database table.
+            I first want to try the basic approach, and then the more complex.
+            """
+            # for level1, level1_dict in vote['votes'][0].items():
+            #     for level2, level2_dict in level1_dict.items():
+            #         for section, section_dict in level2_dict.items():
+            #             for domain, domain_dict in section_dict.items():
+            #                 for program, program_dict in domain_dict.items():
+            #                     for regulation, regulation_dict in program_dict.items():
+            #                         for category, category_dict in regulation_dict.items():
+            #                             total = category_dict['total']
+            #                             query = f"INSERT INTO votes (user_id, level_1, level_2, section, domain, program, regulation, category, total) VALUES ({user_id}, '{level1}', '{level2}', '{section}', '{domain}', '{program}', '{regulation}', '{category}', {total})"
+            #                             self.cursor.execute(query)
+                        
             # execute the query and commit the changes to the database
             self.cursor.execute(query)
             self.db.commit()
