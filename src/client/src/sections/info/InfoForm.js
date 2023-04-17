6fbc60013df62574152e47c8ccfdecba1852b2f5
Elhai Mansbach <elhaimn@gmail.com>
2023-04-18 01:51:22 +0300

    Receiving and displaying the info from the server

--- src/client/src/sections/info/InfoForm.js
+++ src/client/src/sections/info/InfoForm.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from 'react'
-
+import { Grid } from '@mui/material';
 import InfoCards from './InfoCards';
 
-export default function InfoForm() {
+const styleCards = {
+  px: 10,
+  boxShadow: 3,
+  textAlign: 'center',
+  marginLeft: 4,
+  marginRight: 2,
+  marginBottom: 4,
+  fontSize: '18px',
+  borderRadius: '50%',
+  backgroundColor: '#007bff',
+  color: 'black',
+  width: '200px',
+  height: '200px',
+  fontFamily: ' system-ui',
+  '&:hover': {
+    color: 'rgb(0,0,205,0.7)',
+    fontSize: '23px',
+  },
+};
 
-  const [table, setTable] = useState();
+export default function InfoForm() {
+  const [table, setTable] = useState({});
   const url = 'http://localhost:5000/peoples_budget/information';
 
   useEffect(() => {
@@ -14,7 +33,7 @@ export default function InfoForm() {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' },
         });
-        const information = await response.json();
+        const information = JSON.parse(await response.json());
         console.log(information);
         setTable(information);
       } catch (error) {
@@ -26,9 +45,10 @@ export default function InfoForm() {
   }, []);
 
   return (
-    <div>
-      <InfoCards />
-    </div>
+    <Grid container >
+    {Object.keys(table).map((key, index) => (
+      <InfoCards keys={key} value={table[key]} index={index} styleCards={styleCards} />
+    ))}
+    </Grid>
   );
 }
-
