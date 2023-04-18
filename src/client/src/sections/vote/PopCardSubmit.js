3c9eab4bfaa6e1b2f65271d4dfd67298bb70d77c
Elhai Mansbach <elhaimn@gmail.com>
2023-04-18 23:51:44 +0300

    Sending the table of votes and the id of the user

--- src/client/src/sections/vote/PopCardSubmit.js
+++ src/client/src/sections/vote/PopCardSubmit.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState,useContext } from 'react';
+import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
@@ -14,9 +14,7 @@ import Box from '@mui/material/Box';
 import LoadingVote from './LoadingVote';
 import UserContext from '../../contexts/UserContext';
 
-
 function PaperComponent(props) {
-
   return (
     <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
       <Paper {...props} />
@@ -26,10 +24,10 @@ function PaperComponent(props) {
 
 export default function PopCardSubmit(props) {
   const id = useContext(UserContext);
-  
   const navigate = useNavigate();
   const [open, setOpen] = React.useState(false);
   const [loading, setLoading] = useState(false);
+  const [table, setTable] = useState(props.tableData);
   const url = 'http://localhost:5000/peoples_budget/voting';
 
   const handleClickOpen = () => {
@@ -41,13 +39,16 @@ export default function PopCardSubmit(props) {
   };
 
   const handleSubmit = async () => {
-    props.setDisplay(false)
+    // removed the table
+    props.setDisplay(false);
     setLoading(true);
+    console.log(props.tableData);
+    console.log(id);
     try {
       const response = await fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( props.tableData, id ),
+        body: JSON.stringify({id, table}),
       });
 
       const responseData = await response.json();
@@ -70,44 +71,48 @@ export default function PopCardSubmit(props) {
     <div>
       {loading && <LoadingVote />}
 
-      {!loading && <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 4 }}>
-        <Button
-          id="voteSubmit"
-          variant="outlined"
-          sx={{ width: '300px', height: '60px', fontWeight: 'bold', fontSize: '25px' }}
-          onClick={handleClickOpen}
-        >
-          Submit
-        </Button>
-      </Box>}
-
-      {!loading && <Dialog
-        open={open}
-        onClose={handleClose}
-        PaperComponent={PaperComponent}
-        aria-labelledby="draggable-dialog-title"
-      >
-        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-          Confirm Vote Submission
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Are you sure you want to submit your vote?
-            <br />
-            Please note that you can only vote once.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Cancel
+      {!loading && (
+        <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 4 }}>
+          <Button
+            id="voteSubmit"
+            variant="outlined"
+            sx={{ width: '300px', height: '60px', fontWeight: 'bold', fontSize: '25px' }}
+            onClick={handleClickOpen}
+          >
+            Submit
           </Button>
-          <Button onClick={handleSubmit}>Submit</Button>
-        </DialogActions>
-      </Dialog>}
+        </Box>
+      )}
+
+      {!loading && (
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          PaperComponent={PaperComponent}
+          aria-labelledby="draggable-dialog-title"
+        >
+          <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+            Confirm Vote Submission
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to submit your vote?
+              <br />
+              Please note that you can only vote once.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button onClick={handleSubmit}>Submit</Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </div>
   );
 }
 
 PopCardSubmit.propTypes = {
-    setDisplay: PropTypes.func.isRequired,
-};  
+  setDisplay: PropTypes.func.isRequired,
+};
